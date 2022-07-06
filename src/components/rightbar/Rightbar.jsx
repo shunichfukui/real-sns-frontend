76cc171a2c5cdc054d2f53a95347bdc515a82b9c
shunich <fukusyun3@gmail.com>
2022-07-06 19:25:30 +0900

    feat: shareコンポーネント追加

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./Rightbar.css"
 
 function Rightbar() {
   return (
-    <div>Rightbar</div>
+    <div className='rightbar'>Rightbar</div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import "./Home.css"
 import Topbar from '../../components/topbar/Topbar';
 import Sidebar from '../../components/sidebar/Sidebar';
-// import Timeline from '../../components/timeline/Timeline';
-// import Rightbar from '../../components/rightbar/Rightbar';
+import Timeline from '../../components/timeline/Timeline';
+import Rightbar from '../../components/rightbar/Rightbar';
 
 export default function Home() {
   return (
@@ -11,8 +11,8 @@ export default function Home() {
       <Topbar />
       <div className="homeContainer">
         <Sidebar />
-        {/* <Timeline />
-        <Rightbar /> */}
+        <Timeline />
+        <Rightbar />
       </div>
     </>
   )
