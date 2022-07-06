d79597346252e82dad1bab1fb6c017104147c78a
shunich <fukusyun3@gmail.com>
2022-07-06 18:00:21 +0900

    feat: サイドバー追加

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import Topbar from '../../components/Topbar/Topbar';
+import "./Home.css"
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+// import Timeline from '../../components/timeline/Timeline';
+// import Rightbar from '../../components/rightbar/Rightbar';
 
 export default function Home() {
   return (
     <>
       <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        {/* <Timeline />
+        <Rightbar /> */}
+      </div>
     </>
   )
 }
