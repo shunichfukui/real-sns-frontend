e57b078ede552307b5d2272a0773033d543c684c
shunich <fukusyun3@gmail.com>
2022-07-08 15:00:27 +0900

    feat: ログイン、新規登録コンポーネント追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Profile from './components/profile/Profile';
+// import Profile from './components/profile/Profile';
 // import Home from './pages/home/Home';
+// import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
   return (
-    <Profile />
+    <Register />
   );
 }
 
